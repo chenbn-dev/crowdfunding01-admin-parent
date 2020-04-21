f87e654e0d4a12e02b4bd40a61a00a2c8bc6ab0d
chbn <284049379@qq.com>
2020-04-22 01:45:16 +0800

    搭建项目基本架构配置jar包及版本

---
+++ crowdfunding04-admin-entity/src/main/java/cn/chenbonian/crowdfunding/entity/Admin.java
@@ -0,0 +1,23 @@
+package cn.chenbonian.crowdfunding.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author chbn
+ * @create 2020-04-21 0:10
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+
+    private long id;
+    private String loginAcct;
+    private String userPswd;
+    private String userName;
+    private String email;
+    private String createTime;
+
+}
